2df6f574f45703db840bb41c410a962c509a06cf
rocky8818 <rickyglz.1318@gmail.com>
2024-09-22 21:41:35 -0600

    🚧 - help section done

---
+++ src/seeds/data.js
@@ -0,0 +1,111 @@
+// src/data/documentation.js
+export const documentation = {
+    "Cómo crear una cuenta": {
+        titulo: "Cómo crear una cuenta",
+        icono: "newaccount",
+        contenido: [
+            {
+                subtitulo: "Bienvenido",
+                texto: "Para todos los ususarios nuevos de Lustrall, crear una cuenta nueva es muy fácil. Una vez que terminas de ver la bienvenida, te pediremos que ingreses el correo que quieres dar de alta en tu cuenta.  Después presiona el botón que dice “Envíame mi código de acceso”. |En unos segundos recibirás tu código de acceso  de cuatro números, en tu correo. Ingresa este código en la siguiente pantalla cuanto se te solicite, y listo!  |Tu correo habrá sido verificado y tendrás acceso a tu cuenta Lustrall. "
+            }, {
+                subtitulo: "Recomendaciones",
+                texto: "Te recomendamos utilizar un correo al que tengas acceso desde el celular en el que estás usando Lustrall."
+            }
+        ]
+    },
+    "Cómo eliminar tu cuenta": {
+        titulo: "Cómo eliminar tu cuenta",
+        icono: "deleteaccount",
+        contenido: [
+            {
+                subtitulo: "Requisitos",
+                texto: "Para instalar necesitas cumplir con los siguientes requisitos..."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }
+        ]
+    },
+    "Sin contraseñas": {
+        titulo: "Sin contraseñas",
+        icono: "nopasswords",
+        contenido: [
+            {
+                subtitulo: "Lista de componentes",
+                texto: "Aquí encontrarás información detallada sobre los componentes disponibles..."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }
+        ]
+    },
+    "Creando tu invitación en línea": {
+        titulo: "Creando tu invitación en línea",
+        icono: "landing",
+        contenido: [
+            {
+                subtitulo: "Guía de estilos",
+                texto: "En esta sección se detallan las reglas de estilos y cómo aplicarlas."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }
+        ]
+    },
+    "Cómo crear un respaldo": {
+        titulo: "Cómo crear un respaldo",
+        icono: "backup",
+        contenido: [
+            {
+                subtitulo: "Guía de estilos",
+                texto: "En esta sección se detallan las reglas de estilos y cómo aplicarlas."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }
+        ]
+    },
+    "Contratar o cancelar premium": {
+        titulo: "Contratar o cancelar premium",
+        icono: "premium",
+        contenido: [
+            {
+                subtitulo: "Guía de estilos",
+                texto: "En esta sección se detallan las reglas de estilos y cómo aplicarlas."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }, {
+                subtitulo: "Bienvenido",
+                texto: "Esta es la sección de introducción de la documentación."
+            }
+        ]
+    }
+};
